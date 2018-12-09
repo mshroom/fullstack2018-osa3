a9d0d7e22a4423130d7efc510ece25171b2415cc
mshroom <32199029+mshroom@users.noreply.github.com>
2018-12-09 19:12:23 +0200

    ex 3.2

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    let r = '<p>puhelinluettelossa on ' + persons.length + ' henkilön tiedot</p><p>' + new Date() + '</p>'
+    res.send(r)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
