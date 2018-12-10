38080a252be675c586b66bb1d79546790054eb4c
mshroom <32199029+mshroom@users.noreply.github.com>
2018-12-10 23:34:39 +0200

    configured lint to check style

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const cors = require('cors')
 const Person = require('./models/person')
 
 const formatPerson = (person) => {
-    return {
-        name: person.name,
-        number: person.number,
-        id: person._id
-    }
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id
+  }
 }
 
 app.use(morgan('tiny'))
@@ -19,103 +19,98 @@ app.use(cors())
 app.use(express.static('build'))
 
 let persons = [
-    {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-    },
-    {
-    "name": "Martti Tienari",
-    "number": "040-123456",
-    "id": 2
-    },
-    {
-    "name": "Arto Järvinen",
-    "number": "040-123456",
-    "id": 3
-    },
-    {
-    "name": "Lea Kutvonen",
-    "number": "040-123456",
-    "id": 4
-    },
-    {
-    "name": "Testi",
-    "number": "999",
-    "id": 5
-    }
+  {
+    'name': 'Arto Hellas',
+    'number': '040-123456',
+    'id': 1
+  },
+  {
+    'name': 'Martti Tienari',
+    'number': '040-123456',
+    'id': 2
+  },
+  {
+    'name': 'Arto Järvinen',
+    'number': '040-123456',
+    'id': 3
+  },
+  {
+    'name': 'Lea Kutvonen',
+    'number': '040-123456',
+    'id': 4
+  },
+  {
+    'name': 'Testi',
+    'number': '999',
+    'id': 5
+  }
 ]
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
-  
-app.get('/api/persons', (req, res) => {
-    Person
-        .find({})
-        .then(persons => {
-            res.json(persons.map(formatPerson))
-        })
-        .catch(error => {
-            console.log(error)
-            res.status(404).end()
-        })
+  res.send('<h1>Hello World!</h1>')
 })
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max))
-}
+app.get('/api/persons', (req, res) => {
+  Person
+    .find({})
+    .then(persons => {
+      res.json(persons.map(formatPerson))
+    })
+    .catch(error => {
+      console.log(error)
+      res.status(404).end()
+    })
+})
 
 app.post('/api/persons', (req, res) => {
-    const id = getRandomInt(10000)
-    const body = req.body
-    if ((body.name === undefined) || (body.name === '')) {
-        return res.status(400).json({error: 'name missing'})
-    } else if ((body.number === undefined) || (body.number === '')) {
-        return res.status(400).json({error: 'number missing'})
-    }
-    const person = new Person ({
-        name: body.name,
-        number: body.number
-    })
+  const body = req.body
+  if ((body.name === undefined) || (body.name === '')) {
+    return res.status(400).json({ error: 'name missing' })
+  } else if ((body.number === undefined) || (body.number === '')) {
+    return res.status(400).json({ error: 'number missing' })
+  }
+  const person = new Person ({
+    name: body.name,
+    number: body.number
+  })
 
-    person
-        .save()
-        .then(savedPerson => {
-            res.json(formatPerson(savedPerson))
-        })
-        .catch(error => {
-            console.log(error)
-        })
+  person
+    .save()
+    .then(savedPerson => {
+      res.json(formatPerson(savedPerson))
+    })
+    .catch(error => {
+      console.log(error)
+    })
 })
 
 app.get('/info', (req, res) => {
-    let r = '<p>puhelinluettelossa on ' + persons.length + ' henkilön tiedot</p><p>' + new Date() + '</p>'
-    res.send(r)
+  let r = '<p>puhelinluettelossa on ' + persons.length + ' henkilön tiedot</p><p>' + new Date() + '</p>'
+  res.send(r)
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()       
-    }    
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    Person
-        .findByIdAndRemove(req.params.id)
-        .then(result => {
-            res.status(204).end()
-        })
-        .catch(error => {
-            res.status(400).send({ error: 'malformatted id' })
-        })
+  Person
+    .findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => {
+      res.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
